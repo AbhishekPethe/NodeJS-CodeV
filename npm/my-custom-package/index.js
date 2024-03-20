cb3b0c9a8397f4407382ffe9596cc124d7a993e5
Abhishek <abhishekpethe91@gmail.com>
2024-03-20 19:20:39 +0530

    incomplete npm

---
+++ npm/my-custom-package/index.js
@@ -0,0 +1,6 @@
+function greet() {
+    console.log("Custom Package");                                     
+}
+
+
+module.exports = greet
---
+++ npm/readme.md
@@ -0,0 +1,3 @@
+Section is incomplete
+
+Refer from 56 to 60
